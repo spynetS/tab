6d8eb7bb000ded8f39df3bd0f883a45b5c0fcb9f
Alfred Roos <54032594+spynetS@users.noreply.github.com>
2022-06-13 01:39:06 +0200

    update

--- tab.sh
+++ tab.sh
@@ -5,6 +5,7 @@ config=~/.config/tab.config
 if test -f "$config"; then
     . "$config"
 else
+    echo set your default terminal in the tab.config file in .config folder
     echo terminal=kitty>"$config"
     . "$config"
 fi
