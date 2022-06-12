d1e7a3489e1451635d85e59269ad017cb1d6d85b
Alfred Roos <54032594+spynetS@users.noreply.github.com>
2022-06-13 00:30:43 +0200

    added config support

--- tab.sh
+++ tab.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-. ~/.config/tab.config
+
+config=~/.config/tab.config
+
+
+if test -f "$config"; then
+    . "$config"
+else
+    echo terminal=kitty>"$config"
+    . "$config"
+fi
+
 
 place = pwd
 exec "$terminal" $place
